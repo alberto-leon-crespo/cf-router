c9db37cf49e891067e4f27ca6801f45ed67cc596
Alberto León Crespo <alberto.leon.crespo@gmail.com>
2020-03-07 16:18:50 +0100

    Initial Commit

---
+++ decorator/Route.ts
@@ -0,0 +1,26 @@
+import path from "path";
+import {BaseController} from "../../../kernel/BaseController";
+
+export const Route = (method: string, httpPath: string) => {
+    return async (
+        target: any,
+        propertyKey: string,
+        descriptor: PropertyDescriptor
+    ) => {
+        const kernel = await import(path.join(path.resolve("."), process.argv[1]));
+        const router = kernel._Container.get("router");
+        if (descriptor instanceof Promise) {
+            descriptor = await descriptor;
+        }
+        if (descriptor && descriptor.value) {
+            const controller = class extends BaseController {
+                public [descriptor.value.name] = descriptor.value;
+            };
+            const controllerInstance = new controller();
+            controllerInstance.setContainer(kernel._Container);
+            router.registerRoute(method, httpPath, controllerInstance, descriptor.value.name);
+            return descriptor;
+        }
+        return descriptor;
+    };
+};
---
+++ service/Router.ts
@@ -0,0 +1,66 @@
+import {BaseKernel} from "../../../kernel/BaseKernel";
+import {Server} from "../../../http/Server";
+import path from "path";
+
+export default class BaseRouter {
+
+    private _server: Server;
+
+    public async setServer(kernel: BaseKernel, routerConfig: any[]){
+        this._server = kernel.getServer();
+        for (let routeName in routerConfig) {
+            let [controllerFile, controllerMethod] = routerConfig[routeName].controller.split("::");
+            let controllerFullPath = path.join(kernel.getRootDir(), controllerFile);
+            let controllerName = controllerFile.split("/").pop();
+            let controller = await import(controllerFullPath);
+            let controllerInstance = new controller[controllerName]();
+            controllerInstance.setContainer(kernel.getContainer());
+            this._server.registerRoute(
+                routerConfig[routeName].method,
+                routerConfig[routeName].path,
+                controllerInstance,
+                controllerMethod
+            );
+        }
+    }
+
+    public get(path: string, controller: object, controllerMethod: string){
+        this._server.registerRoute("GET", path, controller, controllerMethod);
+    }
+
+    public post(path: string, controller: object, controllerMethod: string){
+        this._server.registerRoute("POST", path, controller, controllerMethod);
+    }
+
+    public put(path: string, controller: object, controllerMethod: string){
+        this._server.registerRoute("PUT", path, controller, controllerMethod);
+    }
+
+    public delete(path: string, controller: object, controllerMethod: string){
+        this._server.registerRoute("DELETE", path, controller, controllerMethod);
+    }
+
+    public patch(path: string, controller: object, controllerMethod: string){
+        this._server.registerRoute("PATCH", path, controller, controllerMethod);
+    }
+
+    public head(path: string, controller: object, controllerMethod: string){
+        this._server.registerRoute("HEAD", path, controller, controllerMethod);
+    }
+
+    public options(path: string, controller: object, controllerMethod: string){
+        this._server.registerRoute("OPTIONS", path, controller, controllerMethod);
+    }
+
+    public trace(path: string, controller: object, controllerMethod: string){
+        this._server.registerRoute("TRACE", path, controller, controllerMethod);
+    }
+
+    public connect(path: string, controller: object, controllerMethod: string){
+        this._server.registerRoute("CONNECT", path, controller, controllerMethod);
+    }
+
+    private registerRoute(method: string, path: string, controller: object, controllerMethod: string){
+        this._server.registerRoute(method, path, controller, controllerMethod);
+    }
+}
